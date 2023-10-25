b802f229602009f5592e56c67ee12d96df8cc9dc
Aarón Guzmán <140634896+aaronguf@users.noreply.github.com>
2023-10-25 23:43:13 +0000

    Dinamizacion de la visualizacion del botón eliminar; Ajustar el texto muy largo dentro del elemento que lo contiene

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,58 +1,81 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
 const ToDoList = () => {
     const [userInput, setUserInput] = useState("");
     const [task, setTask] = useState([]);
+    const [mouseIn, setMouseIn] = useState([]); 
 
     const pressEnter = (tecla) => {
-        if(tecla === "Enter"){
+        if (tecla === "Enter") {
             addInput();
         }
-
     }
 
-    const addInput =() => {
-        if (userInput !== ""){
-            setTask([...task, userInput]);
+    const addInput = () => {
+        if (userInput !== "") {
+            setTask((prevTask) => [...prevTask, userInput]);
             setUserInput("");
+            setMouseIn((prevMouseIn) => [...prevMouseIn, false]); 
         }
-
     }
 
-  return (
-    <div className='container'>
-        <div class="list-group">
-            <h1 className='text-center'>TO DO LIST</h1>
-  
-  <input name='textInput' type="text" value={userInput} placeholder={userInput === "" ? "Agrega una tarea" : ""} onChange={(e) => setUserInput(e.target.value)} onKeyDown={((e) => pressEnter(e.key))}/>
-  
-  {task.map((item, index) => {
-          return (
-            <div className='d-flex'>
-            <a href='#' class='list-group-item list-group-item-action' key={index}>
-              {item}
-            </a>
-            <button 
-            onClick={() => {
-              const updatedTask = task.filter(
-                (_, i) => index != i);
+    return (
+        <div className='container'>
+            <div className="list-group">
+                <h1 className='text-center'>TO DO LIST</h1>
 
-                setTask(updatedTask);
+                <input
+                    name='textInput'
+                    type="text"
+                    value={userInput}
+                    placeholder={userInput === "" ? "Agrega una tarea" : ""}
+                    onChange={(e) => setUserInput(e.target.value)}
+                    onKeyDown={(e) => pressEnter(e.key, e)}
+                />
 
-            }}><i className="fa-solid fa-trash-can"></i></button>
+                {task.map((item, index) => {
+                    return (
+                        <div
+                            className='d-flex toDoElement'
+                            onMouseEnter={() => setMouseIn(prevMouseIn => {
+                                const newMouseIn = [...prevMouseIn];
+                                newMouseIn[index] = true;
+                                return newMouseIn;
+                            })}
+                            onMouseLeave={() => setMouseIn(prevMouseIn => {
+                                const newMouseIn = [...prevMouseIn];
+                                newMouseIn[index] = false;
+                                return newMouseIn;
+                            })}
+                        >
+                            <a href='#' className='list-group-item list-group-item-action toDoText' key={index}>
+                                {item}
+                            </a>
+                            <button
+                                className={mouseIn[index] ? "showButton" : "hideButton"}
+                                key={index}
+                                onClick={() => {
+                                    const updatedTask = task.filter((_, i) => index !== i);
+                                    setTask(updatedTask);
+                                    setMouseIn(prevMouseIn => {
+                                        const newMouseIn = [...prevMouseIn];
+                                        newMouseIn.splice(index, 1); 
+                                        return newMouseIn;
+                                    });
+                                }}
+                            >
+                                <i className="fa-solid fa-trash-can"></i>
+                            </button>
+                        </div>
+                    );
+                })}
+
+                <a className="list-group-item list-group-item-action disabled" aria-disabled="true">
+                    {task.length === 0 ? "No hay tareas por hacer" : task.length === 1 ? "Una tarea por hacer" : `${task.length} tareas por hacer`}
+                </a>
             </div>
-          );
-          })}
-
-
-
-<a className="list-group-item list-group-item-action disabled" aria-disabled="true">
-  {task.length === 0 ? "No hay tareas por hacer" : task.length === 1 ? "Una tarea por hacer" : `${task.length} tareas por hacer`}
-</a>
-</div>
-      
-    </div>
-  )
+        </div>
+    )
 }
 
-export default ToDoList
+export default ToDoList;
